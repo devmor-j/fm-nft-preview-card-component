28a35d3ccd3f8ba04139094df2af7a070fd0febe
Morteza Jamshidi <devmor.j@gmail.com>
2022-03-17 17:28:43 +0330

    📦 NEW: Add custom styles to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "soft-blue": "hsl(215, 51%, 70%)",
+        "cyan:": "hsl(178, 100%, 50%)",
+        "dark-blue-main": "hsl(217, 54%, 11%)",
+        "dark-blue-card": "hsl(216, 50%, 16%)",
+        "dark-blue-line": "hsl(215, 32%, 27%)",
+        "white": "hsl(0, 0%, 100%)",
+      },
+      fontFamily: {
+        Outfit: ['Outfit', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
