6c5293f0df841b5a81791a0b12bb1331db4438f9
Morteza Jamshidi <devmor.j@gmail.com>
2022-03-17 17:44:49 +0330

    👌 IMPROVE: Add prefix to tailwind custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "soft-blue": "hsl(215, 51%, 70%)",
-        "cyan:": "hsl(178, 100%, 50%)",
-        "dark-blue-main": "hsl(217, 54%, 11%)",
-        "dark-blue-card": "hsl(216, 50%, 16%)",
-        "dark-blue-line": "hsl(215, 32%, 27%)",
-        "white": "hsl(0, 0%, 100%)",
+        "nft-soft-blue": "hsl(215, 51%, 70%)",
+        "nft-cyan": "hsl(178, 100%, 50%)",
+        "nft-dark-blue-main": "hsl(217, 54%, 11%)",
+        "nft-dark-blue-card": "hsl(216, 50%, 16%)",
+        "nft-dark-blue-line": "hsl(215, 32%, 27%)",
+        "nft-white": "hsl(0, 0%, 100%)",
       },
       fontFamily: {
         Outfit: ['Outfit', 'sans-serif'],
